b2473862d8467397be36b855a7d478418d6db916
Nawazuddin MohammedKhaja <kfnawaz@gmail.com>
2020-10-24 20:26:42 -0500

    Updates

---
+++ precache-manifest.01ecb1598803d14e76b9a2ee5d70334e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda371c186c019824a34f1ac3872bfcc",
+    "url": "/realnest/index.html"
+  },
+  {
+    "revision": "6337896b643e0a5fe235",
+    "url": "/realnest/static/css/main.99e75adc.chunk.css"
+  },
+  {
+    "revision": "dc1a90743b584734845f",
+    "url": "/realnest/static/js/2.39477723.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/realnest/static/js/2.39477723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6337896b643e0a5fe235",
+    "url": "/realnest/static/js/main.08b85242.chunk.js"
+  },
+  {
+    "revision": "033a46006405f36331a1",
+    "url": "/realnest/static/js/runtime-main.f2fbafed.js"
+  },
+  {
+    "revision": "75e3d4f71853730daa2f29ffda803217",
+    "url": "/realnest/static/media/home2.75e3d4f7.png"
+  },
+  {
+    "revision": "715313cd4f8b7b826663309b8751bb6b",
+    "url": "/realnest/static/media/svg-1.715313cd.svg"
+  },
+  {
+    "revision": "d9e440a5ed9283a035d51b4cb93663f0",
+    "url": "/realnest/static/media/svg-3.d9e440a5.svg"
+  },
+  {
+    "revision": "494b2abdfb54672b38d5c6135a33eda5",
+    "url": "/realnest/static/media/svg-5.494b2abd.svg"
+  },
+  {
+    "revision": "4038bb4e8b2edf33bfb8542d7ef7598e",
+    "url": "/realnest/static/media/ticketVase.4038bb4e.png"
+  },
+  {
+    "revision": "72ed571599b33a2d2810daa85d57e890",
+    "url": "/realnest/static/media/video.72ed5715.mp4"
+  }
+]);
