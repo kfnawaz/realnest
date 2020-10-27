de5a43177b2dda1f4290875299da6035245f448f
Nawazuddin MohammedKhaja <kfnawaz@gmail.com>
2020-10-26 23:09:48 -0500

    fixed the slider text alignment

--- src/components/InfoSlider/index.js
+++ src/components/InfoSlider/index.js
@@ -10,9 +10,12 @@ function InfoSlider() {
 			<Slider classNames={horizontalCss} autoplay={3000}>
 				{content.map((item, index) => (
 					<div key={index} style={{ background: `url('${item.image}') no-repeat center center` }}>
-						<div className="center">
+						{/* <div className={styles.center}> */}
+						<div style={{ textAlign: "center", alignContent: "center", position: "absolute", top: "25%", left: "50%", transform: "translate(-50%, -50%)",}} >
 							<h1>{item.title}</h1>
+							<br />
 							<p>{item.description}</p>
+							<br />
 							<button>{item.button}</button>
 						</div>
 					</div>
