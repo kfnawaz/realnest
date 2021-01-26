6423c19bc66fbf69c4a95c9dc7bdd23f588e8db0
Nawazuddin MohammedKhaja <kfnawaz@gmail.com>
2021-01-26 11:37:42 -0600

    corrected the images

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,3 +1,6 @@
+import homepic from "../../images/home2.png";
+import sharepic from "../../images/share.jpg";
+
 export const homeObjOne = {
   id: "about",
   lightBg: false,
@@ -17,7 +20,7 @@ export const homeObjOne = {
   buttonLabel: "Know More",
   imgStart: false,
   // img: require('../../images/svg-1.svg'),
-  img: require("../../images/home2.png"),
+  img: homepic,
   alt: "Car",
   dark: true,
   primary: true,
@@ -38,7 +41,7 @@ export const homeObjTwo = {
   buttonLabel: "Learn More",
   imgStart: false,
   // img: require('../../images/svg-2.svg'),
-  img: require("../../images/share.jpg"),
+  img: sharepic,
   alt: "Piggybank",
   dark: false,
   primary: false,
