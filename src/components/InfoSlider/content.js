90948ea35a23ad190aaa56d4b8ddcb7a1b73246c
Nawazuddin MohammedKhaja <kfnawaz@gmail.com>
2021-01-29 12:21:50 -0600

    buttons update

--- src/components/InfoSlider/content.js
+++ src/components/InfoSlider/content.js
@@ -15,9 +15,9 @@ export default [
     title: "Tortor Dapibus",
     description:
       "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.",
-    button: "Project Progress",
-    buttontwo: "HAR Listing",
-    buttonthree: "I AM GORGEOUS INSIDE",
+    button: "PROGRESS",
+    buttontwo: "HAR",
+    buttonthree: "GORGEOUS",
     image: car3,
     user: "Samantha",
     userProfile: "https://s7.postimg.cc/ujy8zz7vv/5_3x.png",
